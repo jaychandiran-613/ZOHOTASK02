49adf48c74d1cc9b872ba4412e837a8be7898646
jaychandiran-613 <jayachandiran613@gmail.com>
2022-05-09 17:05:23 +0530

    added muttex

--- file1.cpp
+++ file1.cpp
@@ -1,62 +1,55 @@
 #include<iostream>
-#include<pthread.h>
+#include<thread>
 #include<unistd.h>
 #include<time.h>
 #include<string.h>
+#include<semaphore.h>
+#include<mutex>
 using namespace std;
 #define Thread_size 2
-int buffer;
-void* producer(void* args)
+int buffer[10];
+int count = 0;
+mutex mt;
+sem_t semEmpty;
+sem_t semFull;
+void* producer()
 {   string sample;
     while (1)
     
-    {
+    {   
         int x = rand() % 10;
-        buffer = x;
-       // cout<<"generator : " <<buffer<<endl;
+        sleep(3);
+        sem_wait(&semEmpty);
+        mt.lock();
+        buffer[count] = x;
+        count++;
+        mt.unlock();
+        sem_post(&semFull);
+        cout<<"generator : " <<x<<endl;
+        
     }
     
     
     
 }
-void* consumer(void* args)
+void* consumer()
 {   
     while (1)
-    {
-        
-        sleep(1);
-        cout<<"The consumer received the variable : "<<buffer<<endl;
+    {  int y;
+        sem_wait(&semFull);
+        mt.lock();
+        y = buffer[count -1];
+        count--;
+        mt.unlock();
+        sem_post(&semEmpty);
+        cout<<"The consumer received the variable : "<<y<<endl;
+        sleep(3);
     }
-    
-    
-    
 }
 int main()
 {   srand(time(NULL));
-    pthread_t pt[Thread_size];
-    for(int i=0;i<Thread_size;i++)
-    {
-        if(i==0)
-        {
-            if(pthread_create(&pt[i],NULL,&producer,NULL) != 0)
-            {
-                cout<<"error in creating generator\n";
-            }
-        }
-        else
-        {
-            if(pthread_create(&pt[i],NULL,&consumer,NULL) != 0)
-            {
-                cout<<"error in creating consumer\n";
-            }
-        }
-    }
-    for(int i=0;i<Thread_size;i++)
-    {
-        if(pthread_join(pt[i],NULL) != 0)
-        {
-            cout<<"error in connecting\n";
-        }
-    
-    }
+    thread pt1 (&producer);
+    thread pt (&consumer);
+    pt1.join();
+    pt.join();
 }
